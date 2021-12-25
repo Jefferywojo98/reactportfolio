8f30ea37fc9bd962f16c9a2e9aced1e8ed6f2a25
Jeffery <jefferywojo1998@gmail.com>
2021-12-25 15:32:33 -0600

    added the list and working on nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react'
-import './App.css';
+import NavBar from './components/NavBar/NavBar';
+import About from './components/About/About';
+import Home from './components/Home/Home';
+import Skills from './components/Skills/Skills';
+import Contact from './components/Contact/Contact';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
-  );
+<div className='App'>
+<NavBar />
+<Home />
+<About />
+<Skills />
+<Contact />
+</div>
+  )
 }
 
 export default App;
