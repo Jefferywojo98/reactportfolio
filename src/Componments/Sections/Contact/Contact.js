7d0f4ff69b0915cd16a57950eacc497a81f61c87
Jeffery <jefferywojo1998@gmail.com>
2021-12-28 14:11:51 -0600

    pages are almost done. need to work on resume and styling

--- src/Componments/Sections/Contact/Contact.js
+++ src/Componments/Sections/Contact/Contact.js
@@ -1,13 +1,105 @@
-import React from "react";
-const Contact = () => {
-    return ( 
-        <div>
-            <p> Test</p>
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import { validateEmail } from "./Helper";
+function Contact() {
+    const [email, setEmail] = useState("");
+    const [FullName, setFullName] = useState("");
+    const [Message, setMessage] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+  
+    const handleInputChange = (event) => {
+      const { target } = event;
+      const inputType = target.name;
+      const inputValue = target.value;
+  
+      if (inputType === "email") {
+        setEmail(inputValue);
+      } else if (inputType === "FullName") {
+        setFullName(inputValue);
+      } else {
+        setMessage(inputValue);
+      }
+    };
+  
+    const handleFormSubmit = (event) => {
+      event.preventDefault();
+      if (!validateEmail(email)) {
+        setErrorMessage("Email is invalid");
+        return;
+      }
+      if (!FullName) {
+        setErrorMessage("Name is required");
+  
+        return;
+      }
+      if (!Message) {
+        setErrorMessage("Message is required");
+  
+        return;
+      }
+      alert(`${FullName}, thanks for send me a message!`);
+  
+      setFullName("");
+      setMessage("");
+      setEmail("");
+    };
+  
+    return (
+      <div>
+        <h1 className="text-center">Contact Page</h1>
+        <div className="formCont">
+          <div className="formStyle">
+            <Form>
+              <Form.Group className="text-center">
+                <Form.Label >Name:</Form.Label>
+                <Form.Control
+                  value={FullName}
+                  name="Full Name"
+                  onChange={handleInputChange}
+                  type="text"
+                  placeholder="Full Name"
+                />
+              </Form.Group>
+              <Form.Group className="text-center">
+                <Form.Label>Email Address:</Form.Label>
+                <Form.Control
+                  value={email}
+                  name="email"
+                  onChange={handleInputChange}
+                  type="email"
+                  placeholder="Email Address"
+                />
+              </Form.Group>
+              <Form.Group className="text-center">
+                <Form.Label>Message:</Form.Label>
+                <Form.Control
+                  value={Message}
+                  name="message"
+                  onChange={handleInputChange}
+                  type="text"
+                  placeholder="Send me a message!"
+                  as="textarea"
+                  size="lg"
+                  rows={3}
+                />
+              </Form.Group>
+            </Form>
+            <Button className="justify-content-center"
+              type="button"
+              onClick={handleFormSubmit}
+              variant="outline-light"
+            >
+              Submit
+            </Button>
+            {errorMessage && (
+              <div>
+                <p className="errorText">{errorMessage}</p>
+              </div>
+            )}
+          </div>
         </div>
-     );
-}
- 
-export default Contact;
-<div>
-    <p> Test</p>
-</div>
+      </div>
+    );
+  }
+  
+  export default Contact;
--- src/Componments/Sections/Portfolio/Portfolio.js
+++ src/Componments/Sections/Portfolio/Portfolio.js
@@ -1,8 +1,44 @@
 import React from "react";
+import "./Portfolio.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import project1 from "../../../Images/Gas-Buddy.png";
+import project2 from "../../../Images/views.png"
+import project3 from "../../../Images/password.png"
 const Portfolio = () => {
     return ( 
-        <div>
-            <p> Test</p>
+        <div class="projectcontainer">
+            <div class="project" id="project">
+                <h1 className="text-center">Projects</h1>
+                <div class="container">
+                    <p class ="projectTitle" className="text-center">Gas Buddy</p>
+                    <img class="project1" src={project1}></img>
+                    <p className="text-center"> GitHub Repository:{" "}
+                        <a target="blank"href="https://github.com/Jefferywojo98/Find-my-gas-price">Open here!</a>
+                    </p>
+                     <p className="text-center">
+                    Deploy App:{" "}<a target="blank" href="https://jefferywojo98.github.io/Find-my-gas-price/">Open here!</a>
+                    </p>
+
+                    <p class ="projectTitle1" className="text-center">password Generator</p>
+                    <img class="project3" src={project3}></img>
+                    <p className="text-center"> GitHub Repository:{" "}
+                        <a target="blank"href="https://github.com/Jefferywojo98/Password_Generator">Open here!</a>
+                    </p>
+                     <p className="text-center">
+                    Deploy App:{" "}<a target="blank" href="https://jefferywojo98.github.io/Password_Generator/">Open here!</a>
+                    </p>
+
+                    <p class ="projectTitle" className="text-center">v.i.e.w.s</p>
+                    <img class="project2" src={project2}></img>
+                    <p className="text-center"> GitHub Repository:{" "}
+                        <a target="blank"href="https://github.com/jcapadocia3/v.i.e.w.s.">Open here!</a>
+                    </p>
+                     <p className="text-center">
+                    Deploy App:{" "}<a target="blank" href="https://peaked-project.herokuapp.com/">Open here!</a>
+                    </p>
+                </div>
+
+            </div>
         </div>
      );
 }
